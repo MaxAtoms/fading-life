77cb583469270be892a9caa2e7c7c9cdeb9019c4
Chris <82648581+Ateyu@users.noreply.github.com>
2025-05-24 13:12:44 +0200

    snapshot

---
+++ gameElements/life_orb.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var life_zone: PackedScene
+
+
+func destroy():
+	var pos = global_position
+
+	if life_zone:
+		var new_instance = life_zone.instantiate()
+		get_parent().add_child(new_instance)
+		new_instance.global_position = pos
+		
+	queue_free()
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	destroy()
